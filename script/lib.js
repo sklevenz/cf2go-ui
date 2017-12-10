986d0bd1c8b2a496a1d9f4d723da065361d669c9
Stephan Klevenz <stephan@klaeff.de>
2017-12-10 12:52:58 +0100

    dev

---
+++ script/lib.js
@@ -0,0 +1,16 @@
+  function addRow(content, morecontent) {
+    if (!document.getElementsByTagName) return;
+    tabBody = document.getElementsByTagName("tbody").item(0);
+    row = document.createElement("tr");
+    cell1 = document.createElement("td");
+    cell2 = document.createElement("td");
+    textnode1 = document.createTextNode(content);
+    textnode2 = document.createTextNode(morecontent);
+    cell1.appendChild(textnode1);
+    cell2.appendChild(textnode2);
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    tabBody.appendChild(row);
+
+
+  }
